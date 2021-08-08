9c00199c400767a6d32154c50dd4471f05c36bab
elijahguy <87336860+elijahguy@users.noreply.github.com>
2021-08-08 18:35:56 -0400

    Add files via upload

---
+++ week2hw2.js
@@ -0,0 +1,18 @@
+// Define a funtion maxOFTwoNumbers that takes two numbers as arguments and returns the largest of them.
+function max(firstNum, secondNum){
+  
+    if (firstNum > secondNum) {
+       console.log(firstNum + " is larger than " + secondNum);
+    } else {
+        console.log(firstNum + " is less than " + secondNum);
+    }
+    return;
+  
+}
+
+// two numbers 
+max (86,150)
+
+
+
+
